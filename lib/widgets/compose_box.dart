d1906e14ee018e24794db2fcd2bae48aea45d3b9
Zixuan James Li <zixuan@zulip.com>
2024-11-19 14:23:06 -0500

    compose: Disable compose box widgets when sending message
    Also clear the content input only after the message is successfully
    sent.
    
    For the send button, unlike when there are validation errors, having
    an error dialog when its disabled for sending a message feels a bit
    redundant and doesn't help much.  This can be annoying when the user
    misclicks and hit the send button multiple times.
    
    This implements some behavior not specified in Figma. When disabled:
    
    - Because the height of the progress indicator is 2px, to avoid shifting
      the content of the compose box, we expand it by 2px in height instead
      of 1px.
    
    - The bar containing only buttons other than the send button is faded,
      because the send button can be disabled separately through validation
      errors.  If we need to disable the other compose buttons individually in the
      future, we can control the styling of the buttons instead.
    
    The progress indicator will be added in a later commit.
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/widgets/compose_box.dart
+++ lib/widgets/compose_box.dart
@@ -401,45 +401,47 @@ class _ContentInputState extends State<_ContentInput> with WidgetsBindingObserve
       narrow: widget.narrow,
       controller: widget.controller,
       focusNode: widget.focusNode,
-      fieldViewBuilder: (context) => ConstrainedBox(
-        constraints: BoxConstraints(maxHeight: maxHeight(context)),
-        // This [ClipRect] replaces the [TextField] clipping we disable below.
-        child: ClipRect(
-          child: InsetShadowBox(
-            top: _verticalPadding, bottom: _verticalPadding,
-            color: designVariables.composeBoxBg,
-            child: TextField(
-              enabled: widget.enabled,
-              controller: widget.controller,
-              focusNode: widget.focusNode,
-              // Let the content show through the `contentPadding` so that
-              // our [InsetShadowBox] can fade it smoothly there.
-              clipBehavior: Clip.none,
-              style: TextStyle(
-                fontSize: _fontSize,
-                height: _lineHeightRatio,
-                color: designVariables.textInput),
-              // From the spec at
-              //   https://www.figma.com/design/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=3960-5147&node-type=text&m=dev
-              // > Compose box has the height to fit 2 lines. This is [done] to
-              // > have a bigger hit area for the user to start the input. […]
-              minLines: 2,
-              maxLines: null,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: InputDecoration(
-                // This padding ensures that the user can always scroll long
-                // content entirely out of the top or bottom shadow if desired.
-                // With this and the `minLines: 2` above, an empty content input
-                // gets 60px vertical distance (with no text-size scaling)
-                // between the top of the top shadow and the bottom of the
-                // bottom shadow. That's a bit more than the 54px given in the
-                // Figma, and we can revisit if needed, but it's tricky to get
-                // that 54px distance while also making the scrolling work like
-                // this and offering two lines of touchable area.
-                contentPadding: const EdgeInsets.symmetric(vertical: _verticalPadding),
-                hintText: widget.hintText,
-                hintStyle: TextStyle(
-                  color: designVariables.textInput.withFadedAlpha(0.5))))))));
+      fieldViewBuilder: (context) => Opacity(
+        opacity: widget.enabled ? 1 : 0.5,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: maxHeight(context)),
+          // This [ClipRect] replaces the [TextField] clipping we disable below.
+          child: ClipRect(
+            child: InsetShadowBox(
+              top: _verticalPadding, bottom: _verticalPadding,
+              color: designVariables.composeBoxBg,
+              child: TextField(
+                enabled: widget.enabled,
+                controller: widget.controller,
+                focusNode: widget.focusNode,
+                // Let the content show through the `contentPadding` so that
+                // our [InsetShadowBox] can fade it smoothly there.
+                clipBehavior: Clip.none,
+                style: TextStyle(
+                  fontSize: _fontSize,
+                  height: _lineHeightRatio,
+                  color: designVariables.textInput),
+                // From the spec at
+                //   https://www.figma.com/design/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=3960-5147&node-type=text&m=dev
+                // > Compose box has the height to fit 2 lines. This is [done] to
+                // > have a bigger hit area for the user to start the input. […]
+                minLines: 2,
+                maxLines: null,
+                textCapitalization: TextCapitalization.sentences,
+                decoration: InputDecoration(
+                  // This padding ensures that the user can always scroll long
+                  // content entirely out of the top or bottom shadow if desired.
+                  // With this and the `minLines: 2` above, an empty content input
+                  // gets 60px vertical distance (with no text-size scaling)
+                  // between the top of the top shadow and the bottom of the
+                  // bottom shadow. That's a bit more than the 54px given in the
+                  // Figma, and we can revisit if needed, but it's tricky to get
+                  // that 54px distance while also making the scrolling work like
+                  // this and offering two lines of touchable area.
+                  contentPadding: const EdgeInsets.symmetric(vertical: _verticalPadding),
+                  hintText: widget.hintText,
+                  hintStyle: TextStyle(
+                    color: designVariables.textInput.withFadedAlpha(0.5)))))))));
   }
 }
 
@@ -539,21 +541,24 @@ class _TopicInput extends StatelessWidget {
       controller: controller,
       focusNode: focusNode,
       contentFocusNode: contentFocusNode,
-      fieldViewBuilder: (context) => Container(
-        padding: const EdgeInsets.only(top: 10, bottom: 9),
-        decoration: BoxDecoration(border: Border(bottom: BorderSide(
-          width: 1,
-          color: designVariables.foreground.withFadedAlpha(0.2)))),
-        child: TextField(
-          enabled: enabled,
-          controller: controller,
-          focusNode: focusNode,
-          textInputAction: TextInputAction.next,
-          style: topicTextStyle,
-          decoration: InputDecoration(
-            hintText: zulipLocalizations.composeBoxTopicHintText,
-            hintStyle: topicTextStyle.copyWith(
-              color: designVariables.textInput.withFadedAlpha(0.5))))));
+      fieldViewBuilder: (context) => Opacity(
+        opacity: (enabled) ? 1 : 0.5,
+        child: Container(
+          padding: const EdgeInsets.only(top: 10, bottom: 9),
+          decoration: BoxDecoration(border: Border(bottom: BorderSide(
+            width: 1,
+            color: designVariables.foreground.withFadedAlpha(0.2)))),
+          child: TextField(
+            enabled: enabled,
+            controller: controller,
+            focusNode: focusNode,
+            textInputAction: TextInputAction.next,
+            style: topicTextStyle,
+            decoration: InputDecoration(
+              hintText: zulipLocalizations.composeBoxTopicHintText,
+              hintStyle: topicTextStyle.copyWith(
+                color: designVariables.textInput.withFadedAlpha(0.5))))),
+      ));
   }
 }
 
@@ -741,7 +746,7 @@ abstract class _AttachUploadsButton extends StatelessWidget {
       child: IconButton(
         icon: Icon(icon, color: designVariables.foreground.withFadedAlpha(0.5)),
         tooltip: tooltip(zulipLocalizations),
-        onPressed: () => _handlePress(context)));
+        onPressed: (enabled) ? () => _handlePress(context) : null));
   }
 }
 
@@ -929,11 +934,14 @@ class _ComposeButtonBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      _AttachFileButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
-      _AttachMediaButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
-      _AttachFromCameraButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
-    ]);
+    return Opacity(
+      opacity: (enabled) ? 1 : 0.5,
+      child: Row(children: [
+        _AttachFileButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
+        _AttachMediaButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
+        _AttachFromCameraButton(enabled: enabled, contentController: contentController, contentFocusNode: contentFocusNode),
+      ]),
+    );
   }
 }
 
@@ -1009,21 +1017,22 @@ class _SendButtonState extends State<_SendButton> {
         message: validationErrorMessages.join('\n\n'));
       return;
     }
+    if (!widget.enabled.value) {
+      // The send button should have been disabled
+      // by the time the user tries to tap it.
+      assert(false);
+      return;
+    }
 
     final store = PerAccountStoreWidget.of(context);
     final content = widget.contentController.textNormalized;
 
-    widget.contentController.clear();
-    // The following `stoppedComposing` call is currently redundant,
-    // because clearing input sends a "typing stopped" notice.
-    // It will be necessary once we resolve #720.
     store.typingNotifier.stoppedComposing();
+    widget.enabled.value = false;
 
     try {
-      // TODO(#720) clear content input only on success response;
-      //   while waiting, put input(s) and send button into a disabled
-      //   "working on it" state (letting input text be selected for copying).
       await store.sendMessage(destination: widget.getDestination(), content: content);
+      widget.contentController.clear();
     } on ApiRequestException catch (e) {
       if (!mounted) return;
       final zulipLocalizations = ZulipLocalizations.of(context);
@@ -1035,6 +1044,8 @@ class _SendButtonState extends State<_SendButton> {
         title: zulipLocalizations.errorMessageNotSent,
         message: message);
       return;
+    } finally {
+      widget.enabled.value = true;
     }
   }
 
@@ -1043,7 +1054,7 @@ class _SendButtonState extends State<_SendButton> {
     final designVariables = DesignVariables.of(context);
     final zulipLocalizations = ZulipLocalizations.of(context);
 
-    final iconColor = _hasValidationErrors
+    final iconColor = _hasValidationErrors || !widget.enabled.value
       ? designVariables.icon.withFadedAlpha(0.5)
       : designVariables.icon;
 
@@ -1057,7 +1068,7 @@ class _SendButtonState extends State<_SendButton> {
           // ambient [ButtonStyle.overlayColor], where we set the color for
           // the highlight state to match the Figma design.
           color: iconColor),
-        onPressed: _send));
+        onPressed: (widget.enabled.value) ? _send : null));
   }
 }
 
